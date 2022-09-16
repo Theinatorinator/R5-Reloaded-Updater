0fcf77e3c51450bd7601cdde26d46afaa576131d
Theinatorinator <98051141+Theinatorinator@users.noreply.github.com>
2022-09-16 10:49:52 -0600

    Major bug fix silent from params

--- Updater.cmd
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%AutoUpdate.ps1
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'"
