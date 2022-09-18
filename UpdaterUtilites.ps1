c52e2000b344f0f1902ed013b9e3d14e2d1be084
Theinatorinator <98051141+Theinatorinator@users.noreply.github.com>
2022-09-17 22:23:03 -0600

    update

--- UpdaterUtilites.ps1
+++ UpdaterUtilites.ps1
@@ -91,138 +91,23 @@ function CopyDepotFiles {
     }
 }
 
-function DownloadAndExtractAimTrainer {
-    param([bool]$shouldSaveAimTrainerZip)
-    #default params if not included
-    if ($shouldSaveAimTrainerZip=$null) {
-        $shouldSaveAimTrainerZip=$false
-    }
-    If(!(test-path -PathType container "./temp"))
-    {
-        New-Item -ItemType Directory -Path "./temp"
-    }  
-    #check if path created sucsesfully
-    If(!(test-path -PathType container "./temp"))
-    {
-        throw "temp directory missing"
-    } 
-    #check and remove previous aimreqs
-    If(test-path -PathType container "./temp/aimreq")
-    {
-        remove-item -path "./temp/aimreq" -Force -Recurse -ErrorAction SilentlyContinue
-    }  
-    If(test-path -PathType leaf "./temp/aimreq.zip")
-    {
-        remove-item -path "./temp/aimreq.zip" -Force -Recurse -ErrorAction SilentlyContinue
-    } 
-    #check and remove previous AimTrainerScripts
-    If(test-path -PathType container "./temp/r5_aimtrainer-r5_aimtrainer")
-    {
-        remove-item -path "./temp/r5_aimtrainer-r5_aimtrainer" -Force -Recurse -ErrorAction SilentlyContinue
-    }  
-    If(test-path -PathType leaf "./temp/aimTrainerScripts.zip")
-    {
-         remove-item -path "./temp/aimTrainerScripts.zip" -Force -Recurse -ErrorAction SilentlyContinue
-    } 
-    #download latest aim trainer required files
-    curl.exe -L "https://github.com/ColombianGuy/r5_aimtrainer/releases/latest/download/AimTrainerRequiredFiles.zip" --output "./temp/aimreq.zip"
-    #download latest aim trainer Scripts
-    curl.exe -L "https://github.com/ColombianGuy/r5_aimtrainer/archive/refs/heads/r5_aimtrainer.zip" --output "./temp/aimTrainerScripts.zip"
-    #check downloads
-    If(!(test-path -PathType leaf "./temp/aimreq.zip"))
-    {
-        throw "Download Failed"
-    } 
-    If(!(test-path -PathType leaf "./temp/aimTrainerScripts.zip"))
-    {
-        throw "Download Failed"
-    } 
-    #extract AimTrainerFiles   
-    Expand-Archive "./temp/aimreq.zip" -DestinationPath "./temp/aimreq"
-    Expand-Archive "./temp/aimTrainerScripts.zip" -DestinationPath "./temp/"
-    #delete depot zip if requested
-    if (!$shouldSaveAimTrainerZip) {
-        remove-item -path "./temp/aimreq.zip" -Force -Recurse -ErrorAction SilentlyContinue
-        remove-item -path "./temp/aimTrainerScripts.zip" -Force -Recurse -ErrorAction SilentlyContinue
-    }
-    
-}
-
-function CopyAimTrainerFiles {
-    param([bool]$shouldSaveAimTrainerFolders)
-    #default params if not included
-    if ($null -eq $shouldSaveAimTrainerFolders) {
-        $shouldSaveAimTrainerFolders=$false
-    }
-    #check for depot exsistince
-    If(!(test-path -PathType container "./temp/aimreq"))
-    {
-        throw "Missing aimreq files!"
-    }  
-    If(!(test-path -PathType container "./temp/r5_aimtrainer-r5_aimtrainer"))
-    {
-        throw "Missing AimTrainerScripts!"
-    } 
-    #copy files
-    Copy-Item -Force -Recurse -Verbose "./temp/aimreq/*" -Destination "./"
-    Copy-Item -Force -Recurse -Verbose "./temp/r5_aimtrainer-r5_aimtrainer/*" -Destination "./platform/scripts/"
-    #delete depot folder if requested
-    if (!$shouldSaveAimTrainerFolders) {
-        remove-item -path "./temp/aimreq" -Force -Recurse -ErrorAction SilentlyContinue
-        remove-item -path "./temp/r5_aimtrainer-r5_aimtrainer" -Force -Recurse -ErrorAction SilentlyContinue
-    }
-    
-}
-
 function Main {
     param([bool]$silent)
     $saveZips = $false;
     $saveFolders = $false;
-    $flowState = $false;
-    $sdk = $false;
     if ($silent) {
-        DownloadAndExtractDepot $saveZips
-        DownloadAndExtractAimTrainer
-        CopyDepotFiles
-        CopyAimTrainerFiles
-        Write-Host "OPERATION COMPLETE!" -ForegroundColor Green -BackgroundColor Black
-        exit 0
-    }
-    $reply = Read-Host -Prompt "Press S for simple update, press D to enter advanced mode ?[S/D]"
-    if ( $reply -notmatch "[sS]" ) { 
-        $reply = $null
-        $reply = Read-Host -Prompt "Do you want to save the zip files?[y/n]"
-        if ( $reply -match "[yY]" ) { 
-            $saveZips = $true
-        }
-        $reply = $null
-        $reply = Read-Host -Prompt "Do you want to save the extracted zip files?[y/n]"
-        if ( $reply -match "[yY]" ) { 
-            $saveFolders = $true
-        }
+    DownloadAndExtractDepot $saveZips
+    CopyDepotFiles
+    Write-Host "OPERATION COMPLETE!" -ForegroundColor Green -BackgroundColor Black
+    exit 0
     }
     $reply = $null
-    $reply = Read-Host -Prompt "Do you want to Update/Install The Flowstate Aim trainer?[y/n]"
+    $reply = Read-Host -Prompt "Do you want to save the zip files?[y/n]"
     if ( $reply -match "[yY]" ) { 
-        $flowState = $true
+        $saveZips = $true
     }
-    $reply = $null
-    $reply = Read-Host -Prompt "Do you want to Update/Install The r5 SDK?[y/n]"
-    if ( $reply -match "[yY]" ) { 
-        $sdk = $true
-    }
-    if ($sdk) {
-        DownloadAndExtractDepot $saveZips
-    }
-    if ($flowState) {
-        DownloadAndExtractAimTrainer $saveZips
-    }
-    if ($sdk) {
-        CopyDepotFiles $saveFolders
-    }
-    if ($flowState) {
-        CopyAimTrainerFiles $saveFolders  
-    }
+    DownloadAndExtractDepot $saveZips
+    CopyDepotFiles $saveFolders
     Write-Host "OPERATION COMPLETE!" -ForegroundColor Green -BackgroundColor Black
 }
 
